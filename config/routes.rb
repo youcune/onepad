b767b7230f04a82723cf6859e713836230a593e6
Yu Nakanishi <i@youcune.com>
2013-12-06 22:20:57 +0900

    旧URLは再利用しないことになった

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 OnePad::Application.routes.draw do
   root 'pads#new'
   post 'create(.:format)' => 'pads#create', as: :create_pad
-  get ':key(/:revision)(.:format)' => 'pads#show', as: :pad, constraints: { key: /\w{10}|\w{4}-\w{4}/, revision: /\d{4}-\d{4}-\d{4}/ }
-  put ':key(.:format)' => 'pads#update', constraints: { key: /\w{10}|\w{4}-\w{4}/ }
-  get ':key/history(.:format)' => 'pads#history', constraints: { key: /\w{10}|\w{4}-\w{4}/ }
+  get ':key(/:revision)(.:format)' => 'pads#show', as: :pad, constraints: { key: /\w{4}-\w{4}/, revision: /\d{4}-\d{4}-\d{4}/ }
+  put ':key(.:format)' => 'pads#update', constraints: { key: /\w{4}-\w{4}/ }
+  get ':key/history(.:format)' => 'pads#history', constraints: { key: /\w{4}-\w{4}/ }
 end
