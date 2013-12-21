ad0a4feb203fb0bcb317a45efc131126ff2f6398
Yu Nakanishi <i@youcune.com>
2013-12-21 14:02:43 +0900

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 OnePad::Application.routes.draw do
   root 'pads#new'
+  post 'create(.:format)' => 'pads#create', as: :create_pad
   get ':key(/:revision)(.:format)' => 'pads#show', as: :pad, constraints: { key: /\w{4}-\w{4}|\w{10}/, revision: /\d{4}-\d{4}-\d{4}/ }
   put ':key(.:format)' => 'pads#update', constraints: { key: /\w{4}-\w{4}|\w{10}/ }
 end
