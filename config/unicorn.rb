7125c8a5dc8eeb818a9d6a6f5c0eae4db89f0ac6
Yu Nakanishi <i@youcune.com>
2014-03-08 17:53:56 +0900

    updated unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
 env = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'staging'
-listen "/www/onepad/#{env}/tmp/sockets/#{env}.sock"
+listen "/apps/onepad/#{env}/tmp/sockets/#{env}.sock"
 worker_processes 3
-pid "/www/onepad/#{env}/tmp/pids/#{env}.pid"
+pid "/apps/onepad/#{env}/tmp/pids/#{env}.pid"
